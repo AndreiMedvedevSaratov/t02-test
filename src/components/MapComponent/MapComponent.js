5291ffe33cf11e241bfe12f1e7757a5d8fed11ae
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-30 16:59:11 +0400

    feat: update on map

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -3,13 +3,13 @@ import "leaflet-routing-machine";
 
 const MapComponent = () => {
 
-	const startLat = 51.65626386577985;
-	const startLon = 39.20332473597286;
+	const startLat = 51.605375;
+	const startLon = 45.980403;
 
 	const coords = [startLat, startLon];
 
 	return (
-		<MapContainer center={coords} zoom={8} scrollWheelZoom={false}>
+		<MapContainer center={coords} zoom={8} scrollWheelZoom={true}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
